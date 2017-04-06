25e6eba76dd0d58b00b35342f691015ea765dbc8
Parker Gordon <parker.gordon@careerbuilder.com>
2017-04-05 23:40:38 -0400

    Triangle tests passing

--- src/main/scala/TriangleClassifier.scala
+++ src/main/scala/TriangleClassifier.scala
@@ -5,21 +5,22 @@ trait Triangle {
   def side3: Int
 }
 
-case class Isoscelese(side1: Int, side2: Int, side3: Int) extends Triangle
+case class Isosceles(side1: Int, side2: Int, side3: Int) extends Triangle
 case class Equilateral(side1: Int, side2: Int, side3: Int) extends Triangle
 case class Scalene(side1: Int, side2: Int, side3: Int) extends Triangle
 
 object TriangleClassifier extends App {
   type Sides = (Int, Int, Int)
+  private def twoSidesEqual(t: Sides): Boolean = (t._1 == t._2 || t._2 == t._3 || t._1 == t._3)
+  private def allSidesEqual(t: Sides): Boolean = (t._1 == t._2 & t._2 == t._3)
+  private def invalidSides(t: Sides): Boolean = (t._1 <= 0 || t._2 <= 0 || t._3 <= 0)
 
-  def classify(t: Triangle): Triangle = {
-    def twoSidesEqual(t: Sides):Boolean = (t._1 == t._2 || t._2 == t._3 || t._1 == t._3)
-    val sides = (t.side1, t.side2, t.side3)
-
-    sides match {
-      case (s1, s2, s3) if(s1 == s2 && s2 == s3) => Equilateral(s1,s2,s3)
-      case (s1, s2, s3) if(twoSidesEqual(s1,s2,s3)) => Isoscelese(s1,s2,s3)
-      case _ => Scalene(sides._1, sides._2, sides._3)
+  def classify(t: Sides): Triangle = {
+    t match {
+      case s: Sides if(invalidSides(s)) => throw new IllegalArgumentException("All sides must be greater than 0")
+      case s: Sides if(allSidesEqual(s)) => Equilateral(s._1, s._2, s._3)
+      case s: Sides if(twoSidesEqual(s)) => Isosceles(s._1, s._2, s._3)
+      case _ => Scalene(t._1, t._2, t._3)
     }
   }
 }
--- src/test/scala/TriangleClassifierSpec.scala
+++ src/test/scala/TriangleClassifierSpec.scala
@@ -2,16 +2,33 @@ import org.scalatest.{FunSpecLike, Matchers}
 
 class TriangleClassifierSpec extends FunSpecLike with Matchers {
   describe("The TriangleCategorizer utility") {
-    it("should determine equilateral triangle types"){
+    it("should throw an IllegalArgumentException when any sides are <= 0") {
+      val sides = (0,1,1)
 
+      a [IllegalArgumentException] should be thrownBy {
+        val triangle = TriangleClassifier.classify(sides)
+      }
+    }
+
+    it("should determine equilateral triangle types"){
+      val sides = (1,1,1)
+      val triangle = TriangleClassifier.classify(sides)
+
+      triangle shouldBe Equilateral(1,1,1)
     }
 
     it("should determine isosceles triangle types"){
+      val sides = (1,1,2)
+      val triangle = TriangleClassifier.classify(sides)
 
+      triangle shouldBe Isosceles(1,1,2)
     }
 
     it("should determine scalene triangle types"){
+      val sides = (1,2,3)
+      val triangle = TriangleClassifier.classify(sides)
 
+      triangle shouldBe Scalene(1,2,3)
     }
   }
 }
