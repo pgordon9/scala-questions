4dedcd8fa3a837903cf2d9ac534cde2306b4b157
Parker Gordon <parker.gordon@careerbuilder.com>
2017-04-05 23:58:32 -0400

    List traversal tests passing

--- src/main/scala/ListTraversal.scala
+++ src/main/scala/ListTraversal.scala
@@ -1,4 +1,12 @@
+import scala.annotation.tailrec
 
 object ListTraversal {
-
+  @tailrec
+  def fifthElement[T](l: List[T]): T = {
+    l match {
+      case e5::e4::e3::e2::e1::Nil => e5
+      case _::t => fifthElement(t)
+      case _ => throw new IllegalArgumentException("list must contain at least 5 elements")
+    }
+  }
 }
--- src/test/scala/ListTraversalSpec.scala
+++ src/test/scala/ListTraversalSpec.scala
@@ -2,12 +2,20 @@ import org.scalatest.{FunSpecLike, Matchers}
 
 class ListTraversalSpec extends FunSpecLike with Matchers {
   describe("The ListTraversal utility") {
-    describe("when determining the 5th from the last element") {
+    describe("when determining the 5th element from the end of a list") {
       it("should throw an IllegalArgumentException when Lists with less than 5 elements are passed in") {
+        val l = List(4,3,2,1)
+
+        a [IllegalArgumentException] should be thrownBy {
+          val elem = ListTraversal.fifthElement(l)
+        }
       }
 
       it("should determine the 5th element from the end of the list") {
+        val l = List(7,6,5,4,3,2,1)
+        val elem = ListTraversal.fifthElement(l)
 
+        elem shouldBe 5
       }
     }
 
